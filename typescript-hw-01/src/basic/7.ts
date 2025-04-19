09b8936df1b793277b44a1a81cca7a4a99ee7de9
Sofiia Novychenko <milakotiy@gmail.com>
2025-04-19 02:06:31 +0200

    add 'open' | 'close' to status in basic folder for task 7

--- typescript-hw-01/src/basic/7.ts
+++ typescript-hw-01/src/basic/7.ts
@@ -2,7 +2,7 @@ type PageInfo = {
   title: string;
   likes: number;
   accounts: [...string[]];
-  status: string;
+  status: 'open' | 'close';
   details?: {
     createAt: Date;
     updateAt: Date;
